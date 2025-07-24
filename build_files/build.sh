ff5dcef17fdf0a69cf7a537ad1aef5a9fc609d1f
Andrew Clunis <andrew@orospakr.ca>
2025-07-23 21:45:29 -0400

    Define hyprblue image, use hyprland COPR to install hyprland packages on it

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,19 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
 
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
+#
+dnf5 -y copr enable solopasha/hyprland
+
+dnf5 -y install xdg-desktop-portal-hyprland hyprland hyprland-contrib hyprland-plugins hyprpaper hyprpicker hypridle hyprlock pyprland waybar-git
+
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
