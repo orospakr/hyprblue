f936ba0c66a7889d0ed733b7dd8e0ebcae060458
Andrew Clunis <andrew@orospakr.ca>
2025-07-25 22:34:55 -0400

    Revert to unopinionated set of standard hypr* and friends packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,12 +9,10 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
+# While Bluefin like most modern desktops is a pipewire based system, pavycontrol remains useful as a standalone UI, and waybar default config uses it.
+dnf5 -y install pavucontrol
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-#
+# hyprland COPR from solopasha
 dnf5 -y copr enable solopasha/hyprland
 dnf5 -y install xdg-desktop-portal-hyprland hyprland hyprland-contrib hyprland-plugins hyprpaper hyprpicker hypridle hyprshot hyprlock pyprland waybar-git xdg-desktop-portal-hyprland hyprland-qtutils
 
@@ -25,32 +23,7 @@ dnf5 -y install swayosd
 # other related packages found in main Fedora repos:
 dnf5 -y install mako swaybg
 
-## == Other system utilities
-
-# chromium browser. just for use for headless PWAs (web2app)
-dnf5 -y install chromium
-
-# this is a pipewire system of course, but CLI
-dnf5 -y install pavucontrol
-
-# Ghostty
-dnf copr -y enable alternateved/ghostty
-dnf install -y ghostty
-
-## == Additional Development
-
-# install more of the system development packages, including all of the GNOME headers. this allows more local development workflows targetting the local host, of course without compromising host integrity.
-
-# arg the dnf5 groups appear unmaintained. gnome-software-devleopment seems mostly GNOME 3 darp
-dnf5 group install -y --with-optional c-development
-dnf5 group install -y --with-optional gnome-software-development
-
-# gnome-software-development does not include gtk4:
-#dnf5 install -y gkt4-devel-docs gkt4-devel gkt4-devel-docs
-#dnf5 install -y libadwaita-devel
-
-# gnome-software-development installs GNOME Builder, but that should still be gotten from Flatpak.
-dnf5 -y remove gnome-builder
+# TODO: build walker from source.
 
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
